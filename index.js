527f7cab626546da9f48cde726c00f55e4edd339
Joel Clair <clair.joel@yahoo.com>
2018-02-12 15:20:49 -0800

    testing deployment

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+//TODO: once we are ready to deploy replace this info below with the following
+// require('dotenv').config();
+// require('./lib/server').start();
+console.log('HERE in index.js');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 4444));
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+//TODO:for any changes, git push heroku master;

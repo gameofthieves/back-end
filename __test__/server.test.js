6ec2992936f154177ff7b6d9d02d3cb21180f6b0
Mitchell M <mitchellm1618@gmail.com>
2018-02-14 11:07:07 -0800

    more travis testing

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const net = require('net');
-const User = require('../model/user');
-const command = require('../lib/commands');
+// const User = require('../model/user');
+// const command = require('../lib/commands');
 const server = module.exports = net.createServer();
 const PORT = process.env.PORT;
 
-describe('valid requests', () => {
-    it('should listen on port 4000', () => {
-        server.listen(PORT, () => console.log(`Listening on ${PORT}`));
-        expect(PORT).toEqual('4000');
-    });
+describe('#server', () => {
+  beforeAll(server.start);
+  afterAll(server.stop);
+
+  it('should be listening on port 4000', () => {
+    // server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+    expect(PORT).toBe(4000);
+  });
+
+  it('should be on', () => {
+    expect(server.isOn).toBe(true);
+  });
 });

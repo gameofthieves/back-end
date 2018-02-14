f6a230cea7bc3d02d5ffcc63c9dfd58a6b217fe9
Joel Clair <clair.joel@yahoo.com>
2018-02-14 11:35:02 -0800

    mock test built

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -3,12 +3,35 @@
 const net = require('net');
 const User = require('../model/user');
 const command = require('../lib/commands');
-const server = module.exports = net.createServer();
+const server = require('../lib/server');
 const PORT = process.env.PORT;
 
+afterAll(server.stop);
 describe('valid requests', () => {
-    it('should listen on port 4000', () => {
-        server.listen(PORT, () => console.log(`Listening on ${PORT}`));
-        expect(PORT).toEqual('4000');
+  it(`should listen on 3000`, () => {
+    expect(PORT).toEqual('3000');
+  });
+
+  it('should connect and notify me that I joined the channel', done => {
+    let message = [];
+    const client = net.connect({ port: 3000 });
+    // console.log(socket)
+    client.on('data', data => {
+      console.log('DATA', data.toString());
+      message.push(data.toString());
+      client.end(null, () => {
+        console.log('MESSAGE', message);
+        if(message.includes(data.toString())){
+          var testingExpected = true;
+        } else {
+          testingExpected = false;
+        }
+        expect(testingExpected).toBe(true);
+        console.log('MESSAGE', message);
+        done();
+      });
     });
+  });
+
+
 });

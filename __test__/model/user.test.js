80b3caff0e7427c6f8f3e7c7d3e8f6d576625dd7
Joel Clair <clair.joel@yahoo.com>
2018-02-14 13:10:06 -0800

    working on tests of user

--- __test__/model/user.test.js
+++ __test__/model/user.test.js
@@ -1,24 +1,11 @@
 'use strict';
 
-const User = require('../../model/user');
-const server = require('../../lib/server');
+const user = require('../../model/user.js');
+const faker = require('faker')
+const net = require('net')
+const server = require('../..lib/server.js')
 
+describe('user model', () => {
 
-const superagent = require('superagent');
-const net = require('net');
-const PORT = process.env.PORT;
-
-describe('user.js', () => {
-  it('should return a property from the user object', () => {
-
-    let client = net.connect({port: 3000});
-    client.on('connection', data => {
-      console.log('USERDATA', data);
-      client.end();
-    });
-
-    client.on('end', () => {
-      console.log('user test ending');
-    });
-  });
+  describe('')
 });
